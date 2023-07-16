ebc14351e0b5fed33165b885c4b0d65ea8179ee0
Dale Scicluna <dale.scicluna.20@um.edu.mt>
2023-07-16 23:09:12 +0200

    added orientation changer mechanic for control tab and added deisgn effect to the google map fragment

--- app/src/main/java/com/example/daliot/MainActivity.java
+++ app/src/main/java/com/example/daliot/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.daliot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new fragment_home());
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.home){
+
+                loadFragment(new fragment_home());
+                return true;
+            } else if (item.getItemId() == R.id.dashboard) {
+
+                loadFragment(new fragment_dashboard());
+                return true;
+            } else if (item.getItemId() == R.id.control) {
+
+                loadFragment(new fragment_control());
+                return true;
+            } else if (item.getItemId() == R.id.about) {
+
+                loadFragment(new fragment_about());
+                return true;
+            }else{
+
+                return false;
+
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
